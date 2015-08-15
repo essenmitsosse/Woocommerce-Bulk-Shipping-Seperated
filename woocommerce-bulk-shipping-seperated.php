07993d121cae168a300971ff42304fd31c8ad6ca
marcus <marcus@essenmitsosse.de>
2015-08-15 14:19:23 +0200

    initial commit

---
+++ woocommerce-bulk-shipping-seperated.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Plugin Name:     WooCommerce Bulk Shipping seperated per shipping class
+ * Description:     Display different options on check out  for  different shipping classes
+ * Author:          essenmitsosse
+ * Version:         1.0.0
+ * Author URI:      http://essenmitsosse.de
+ * Text Domain:     woocommerce-bulk-shipping-seperated
+ * Upgrade Check:   none
+ * Last Change:     15.08.2015 14:00
+ */
